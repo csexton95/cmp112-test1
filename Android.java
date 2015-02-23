33b8bbf8163a52ce83ac1f97c39c2c3e513aeb78
csexton95 <csexton@sjccs142.saintjoe.edu>
2015-02-23 15:54:29 -0500

    added new comments

--- Android.java
+++ Android.java
@@ -11,19 +11,20 @@ public class Android {
 		year = thisYear;
 		ram = thisRam;
 	}
-
+// Adds the data members make, model, year, and ram
 	Android (String thisMake, String thisModel, int thisYear) {
 		make = thisMake;
 		model = thisModel;	
 		year = thisYear;
 	}
-	
+	// Says is already used the ram and needs to use the other three
 	Android (String thisMake, String thisModel) {
 		make = thisMake;	
 		model = thisModel;
 	}
-
+//Used the year number
 	Android (String thisMake) {
 		make = thisMake;
 	}
 }
+//Used the model number and will use the make when it is finished running
