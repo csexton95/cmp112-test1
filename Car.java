2d7a5ca1a394ce9682c91f04699f84874b4b89f4
csexton95 <csexton@sjccs142.saintjoe.edu>
2015-02-23 15:28:09 -0500

    added new code

--- Car.java
+++ Car.java
@@ -8,19 +8,20 @@ Car () {
 	make = "Unknown";
 	year = 2015;
 	}
-
+// Adds the two class descriptors to teh object car
 Car (String thisMake) {
 	this();
 	make = thisMake;
 	}
-
+// Uses this to refer to the object that is calling the method 
 Car (String thisMake, int thisYear) {
 	make = thisMake;
 	year = thisYear;
 	}
-
+// Uses this to call both methods, make and year, to make them refer to the object
 public String toString() {
 	String desc = "Make: " + make + " Year: " + year;
 	return desc;
 	}
 }
+// Prints out the data
